2885462aced246fe225b6b588d3038232836db84
Adetoye Boluwatife <adetoyeboluwatife01@gmail.com>
2025-04-28 09:51:38 -0500

    Functional Programming

---
+++ src/Emmiter.java
@@ -0,0 +1,11 @@
+public class Emmiter {
+
+    public  static  void print(Object obj){
+         System.out.print(obj +" -> ");
+    }
+
+
+    public  void print2(Object obj){
+        System.out.print(obj +" ----- ");
+    }
+}
--- src/LamdaDemo.java
+++ src/LamdaDemo.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.*;
 
 public class LamdaDemo {
     public  void run(){
@@ -24,6 +27,8 @@ public class LamdaDemo {
 
 
 
+
+
     }
 
     public  int square(int x){
@@ -39,6 +44,87 @@ public class LamdaDemo {
 
         var ans =  func.add(7,9);
         System.out.println(" Ans = "+ans);
+
+        List<Integer> numbers = Arrays.asList(7,4,5,2,9,3);
+        Comparator<Integer> comparator = (x,y) -> y -x;
+
+        numbers.sort(comparator);
+
+        System.out.println(numbers);
+
+
+        Predicate<Integer>  predicate = (x)-> x % 2==0;
+
+        Predicate<Integer> below18 = (x) -> x < 18 ;
+
+        System.out.println("7 is even number  = "+ predicate.test(7));
+
+        System.out.println(" 19 is below 18  = "+ below18.test(19));
+
+
+        Consumer<String> printer = (x)-> System.out.println(x);
+
+        printer.accept("Hello World");
+        printer.accept("This is java ");
+
+        double salary =0.0;
+
+        Function<Integer,Double> compInterest = (loan)-> loan * 0.25 * 6;
+        Function<Integer, Double> comAdvanceIntrest = (loan)-> loan * 5.25 * 20;
+
+        System.out.println("Intrest on loan 50_000 ="+ compInterest.apply(50_000));
+        System.out.println("Intrest on loan 50_000 ="+ comAdvanceIntrest.apply(50_000));
+
+        BinaryOperator<Double> compLoan = (amount,rate)-> (amount * rate * 12) + amount;
+
+        System.out.println("Total payable after 12 months on loan 50_000 ="+ compLoan.apply(50_000.0, 0.25));
+
+        UnaryOperator<Integer>  increment = (x)-> x++;
+        
+        
+        int[] counter ={0};
+        
+        Runnable runnable = ()-> counter[0]++;
+        runnable.run();
+        runnable.run();
+        runnable.run();
+        
+        System.out.println("Counter = "+counter[0]);
+
+
+        List<String> names = Arrays.asList("James","Ben","King","Nothing");
+
+       // names.stream().forEach(System.out::print);
+
+       // names.stream().forEach( (name)-> System.out.print(name));
+
+        //names.stream().forEach(Emmiter::print);
+
+        System.out.println();
+
+        Emmiter emmiter = new Emmiter();
+
+      //  names.stream().forEach(emmiter::print2);
+
+        // Terminal  operation
+        String allName = String.valueOf(names.stream().reduce( (x, y)-> x.concat(" "+y)));
+
+        // Intermediate Operation
+        names.stream()
+                .map(name-> name+"-"+ name.length())
+                .map(name-> name+"-"+ name.length())
+                .map(name-> name+"-"+ name.length())
+                .forEach(Emmiter::print);
+
+        System.out.println(allName.toString());
+
+
+
+
+
+    }
+
+    public  void computeSalary(Consumer<Integer> func){
 
     }
 
